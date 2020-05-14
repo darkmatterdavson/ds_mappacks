f52167bc1fa8f8c2c6c2dfd4284934f690f9983d
DavSonCZE <40635601+DavSonCZE@users.noreply.github.com>
2020-05-14 13:21:56 +0200

    Add files via upload

---
+++ __resource.lua
@@ -0,0 +1,10 @@
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5'
+this_is_a_map 'yes'
+
+author 'DavSonCZE'
+description 'Clear MapPack manager'
+version '1.0.0'
+
+files{
+    'stream/*/*.ymap',
+}
